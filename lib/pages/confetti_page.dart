d58d07ea801fca023ea1682afb9e6cc11d767410
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-05 19:05:45 +0500

    imp

--- lib/pages/confetti_page.dart
+++ lib/pages/confetti_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:confetti/confetti.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,26 +15,59 @@ class _ConfettiPageState extends State<ConfettiPage> {
   final controller = ConfettiController(
     duration: Duration(seconds: 30),
   );
+  Alignment align = Alignment.center;
+  double blast = pi * 0.5;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Stack(
-        alignment: Alignment.centerRight,
+        alignment: align,
         children: [
-          Center(
-            child: CupertinoButton(
-              onPressed: () {
-                controller.stop(clearAllParticles: true);
-                controller.play();
-              },
-              child: Text("Play Confetti"),
-            ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: CupertinoButton(
+                      onPressed: () {
+                        blast = pi * 1.25;
+                        align = Alignment.topLeft;
+                        setState(() {});
+                        controller.stop(clearAllParticles: true);
+                        controller.play();
+                      },
+                      child: Text("Top Left"),
+                    ),
+                  ),
+                  Expanded(
+                    child: CupertinoButton(
+                      onPressed: () {
+                        blast = pi * 1.5;
+                        align = Alignment.topCenter;
+                        setState(() {});
+                        controller.stop(clearAllParticles: true);
+                        controller.play();
+                      },
+                      child: Text("Top Center"),
+                    ),
+                  ),
+                  Expanded(
+                    child: CupertinoButton(
+                      onPressed: () {},
+                      child: Text("Top Right"),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+            ],
           ),
           ConfettiWidget(
             confettiController: controller,
-
+            blastDirection: blast,
             numberOfParticles: 1000,
             gravity: 0.1,
             colors: [
