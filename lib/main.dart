cdf2c3d849a10c020a44ef7b823f79de64f8f601
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-05 17:49:42 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,67 @@
+import 'package:animation_d5/pages/confetti_page.dart';
+import 'package:animation_d5/pages/loading_page.dart';
+import 'package:animation_d5/pages/progress_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoButton(
+                onPressed: () {
+                  Get.to(ConfettiPage());
+                },
+                child: Text("Confetti Page"),
+              ),
+              Divider(),
+              CupertinoButton(
+                onPressed: () {
+                  Get.to(LoadingPage());
+                },
+                child: Text("Loading Page"),
+              ),
+              Divider(),
+              CupertinoButton(
+                onPressed: () {
+                  Get.to(ProgressPage());
+                },
+                child: Text("Progress Page"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/confetti_page.dart
@@ -0,0 +1,34 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ConfettiPage extends StatefulWidget {
+  const ConfettiPage({super.key});
+
+  @override
+  State<ConfettiPage> createState() => _ConfettiPageState();
+}
+
+class _ConfettiPageState extends State<ConfettiPage> {
+  final controller = ConfettiController(duration: Duration(seconds: 2),);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: CupertinoButton(
+                onPressed: () {},
+                child: Text("Play Confetti"),
+              ),
+            ),
+            ConfettiWidget(confettiController:),
+          ],
+        ),
+      ),
+    );
+  }
+}
