fe290969f0d03f526710d92a2ff08450446bb430
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-05 19:11:02 +0500

    imp

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
@@ -10,6 +12,35 @@ class LoadingPage extends StatefulWidget {
 class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: LoadingAnimationWidget.inkDrop(
+                    color: Colors.red,
+                    size: 50,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  CupertinoButton(
+                    onPressed: () {},
+                    child: LoadingAnimationWidget.waveDots(
+                      color: Colors.red,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
