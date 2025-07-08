8d4310b2ea0f32b6d283253901864d0f603621e0
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-08 17:22:55 +0500

    imp

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
@@ -10,62 +8,8 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
-  Widget loading = LoadingAnimationWidget.inkDrop(
-    color: Colors.red,
-    size: 50,
-  );
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: LoadingAnimationWidget.inkDrop(
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {},
-                    child: LoadingAnimationWidget.waveDots(
-                      color: Colors.red,
-                      size: 50,
-                    ),
-                  ),
-                  Spacer(),
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {},
-                    child: LoadingAnimationWidget.inkDrop(
-                      color: Colors.red,
-                      size: 50,
-                    ),
-                  ),
-                  Spacer(),
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {},
-                    child: LoadingAnimationWidget.twistingDots(
-                      size: 50,
-                      leftDotColor: Colors.green,
-                      rightDotColor: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
